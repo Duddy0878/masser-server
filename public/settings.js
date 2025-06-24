5f05974dcc4fdfaf803d34b9ddcc320ab88f4007
David <doubleyoueb@gmail.com>
2025-06-24 18:32:09 -0400

    h

---
+++ public/settings.js
@@ -0,0 +1,16 @@
+
+
+const colorSwitchBtn = document.querySelector('.colorSwitch');
+let isSwapped = false;
+
+colorSwitchBtn.addEventListener('click', () => {
+  if (!isSwapped) {
+    document.documentElement.style.setProperty('--backroundBlue', 'white');
+    document.documentElement.style.setProperty('--mainWhite', 'rgb(41, 173, 190)');
+    isSwapped = true;
+  } else {
+    document.documentElement.style.setProperty('--backroundBlue', 'rgb(41, 173, 190)');
+    document.documentElement.style.setProperty('--mainWhite', 'white');
+    isSwapped = false;
+  }
+});
