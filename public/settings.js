45c76f2d33ceed90dc6972bd947ebf3020ad82dc
David <doubleyoueb@gmail.com>
2025-06-26 13:05:52 -0400

    h

--- public/settings.js
+++ public/settings.js
@@ -5,7 +5,7 @@ let isSwapped = false;
 
 colorSwitchBtn.addEventListener('click', () => {
   if (!isSwapped) {
-    document.documentElement.style.setProperty('--backroundBlue', 'lightgray');
+    document.documentElement.style.setProperty('--backroundBlue', 'rgb(6, 5, 5)');
     document.documentElement.style.setProperty('--mainWhite', 'rgb(41, 173, 190)');
     isSwapped = true;
   } else {
