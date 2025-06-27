bae94b1b60d11c652a1c15c390c55fe7a622196b
David <doubleyoueb@gmail.com>
2025-06-26 23:24:40 -0400

    Update settings.js

--- public/settings.js
+++ public/settings.js
@@ -11,10 +11,10 @@ colorSwitchBtn.addEventListener('click', () => {
   if (localStorage.getItem('isSwapped') === false) {
     document.documentElement.style.setProperty('--backroundBlue', 'rgb(6, 5, 5)');
     document.documentElement.style.setProperty('--mainWhite', 'rgb(41, 173, 190)');
-    localStorage.setItem('isSwapped') === true;
+    localStorage.setItem('isSwapped', true);
   } else {
     document.documentElement.style.setProperty('--backroundBlue', 'rgb(41, 173, 190)');
     document.documentElement.style.setProperty('--mainWhite', 'white');
-    localStorage.setItem('isSwapped') === false;
+    localStorage.setItem('isSwapped', false);
   }
 });
