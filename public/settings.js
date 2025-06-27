31d597b8dd4ad6c1c1b1299a7dac698f8146b2b5
David <doubleyoueb@gmail.com>
2025-06-26 20:54:38 -0400

    Update settings.js

--- public/settings.js
+++ public/settings.js
@@ -1,20 +1,20 @@
 
-localStorage.setItem('isSwapped', false);
+if (localStorage.getItem('isSwapped') === null) {
+  localStorage.setItem('isSwapped', false);
+}
 
 
 const colorSwitchBtn = document.querySelector('.colorSwitch');
-let isSwapped = localStorage.getItem('isSwapped');
 
-console.log(isSwapped);
 
 colorSwitchBtn.addEventListener('click', () => {
-  if (!isSwapped) {
+  if (localStorage.getItem('isSwapped') === false) {
     document.documentElement.style.setProperty('--backroundBlue', 'rgb(6, 5, 5)');
     document.documentElement.style.setProperty('--mainWhite', 'rgb(41, 173, 190)');
-    isSwapped = true;
+    localStorage.getItem('isSwapped') === true;
   } else {
     document.documentElement.style.setProperty('--backroundBlue', 'rgb(41, 173, 190)');
     document.documentElement.style.setProperty('--mainWhite', 'white');
-    isSwapped = false;
+    ilocalStorage.getItem('isSwapped') === false;
   }
 });
