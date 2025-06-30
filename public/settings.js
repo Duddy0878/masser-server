c78ca008f093c5fc343f926661db0084d85d6f66
David <doubleyoueb@gmail.com>
2025-06-29 23:23:44 -0400

    Update settings.js

--- public/settings.js
+++ public/settings.js
@@ -41,13 +41,11 @@ if (localStorage.getItem('isSwapped') === null) {
 
     function displayTheme(){
       if (!isSwapped) {
-    
+         document.documentElement.style.setProperty('--backroundBlue', 'rgb(41, 173, 190)');
+        document.documentElement.style.setProperty('--mainWhite', 'white');
+      } else {
         document.documentElement.style.setProperty('--backroundBlue', 'rgb(6, 5, 5)');
         document.documentElement.style.setProperty('--mainWhite', 'rgb(41, 173, 190)');
-    
-      } else {
-        document.documentElement.style.setProperty('--backroundBlue', 'rgb(41, 173, 190)');
-        document.documentElement.style.setProperty('--mainWhite', 'white');
 
        }
     }
